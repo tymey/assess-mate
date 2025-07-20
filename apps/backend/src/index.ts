e48ca5f1e28bd2808db51a3ec129204236e1149a
tymey <tyler.meyer.a@gmail.com>
2025-07-20 17:04:10 -0500

    feat(auth): Set up routes for signup & login

--- apps/backend/src/index.ts
+++ apps/backend/src/index.ts
@@ -2,11 +2,16 @@ import express from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
 
+import authRoutes from './routes/auth';
+
 dotenv.config();
 
 const app = express();
 app.use(cors());
 app.use(express.json());
+
+// Router
+app.use('/api/auth', authRoutes);
 
 app.get('/', (_, res) => res.send('AssessMate API is running!'));
 
