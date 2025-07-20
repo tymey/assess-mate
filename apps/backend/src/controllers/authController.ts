9b02badedc316a453e9268101e6a826ede60117c
tymey <tyler.meyer.a@gmail.com>
2025-07-20 16:56:46 -0500

    feat(auth): Create auth controller for signup & login

---
+++ apps/backend/src/controllers/authController.ts
@@ -0,0 +1,55 @@
+import { Request, Response } from 'express';
+import bcrypt from 'bcrypt';
+import jwt from 'jsonwebtoken';
+import prisma from '../db/prisma';
+
+const JWT_SECRET = process.env.JWT_SECRET || 'changeme';
+
+export const signup = async (req: Request, res: Response) => {
+    const { username, password, name, email } = req.body;
+
+    if (!username || !password) {
+        return res.status(400).json({ message: "Username and password required" });
+    }
+
+    try {
+        const existingUser = await prisma.user.findUnique({ where: { username } });
+        if (existingUser) {
+            return res.status(409).json({ message: 'Username already taken' });
+        }
+
+        const hashed = await bcrypt.hash(password, 10);
+        const user = await prisma.user.create({ data: {
+            username,
+            password: hashed,
+            name,
+            email
+        }});
+
+        const token = jwt.sign({ userId: user.id }, JWT_SECRET, { expiresIn: '7d' });
+        res.status(201).json({ token, username: user.username });
+    } catch (error) {
+        res.status(500).json({ error: 'Internal server error' });
+    }
+};
+
+export const login = async (req: Request, res: Response) => {
+    const { username, password } = req.body;
+
+    try {
+        const user = await prisma.user.findUnique({ where: { username } });
+        if (!user) {
+            return res.send(401).json({ message: 'Invalid credentials' });
+        }
+
+        const valid = await bcrypt.compare(password, user.password);
+        if (!valid) {
+            return res.status(401).json({ message: 'Invalid credentials' });
+        }
+
+        const token = jwt.sign({ userId: user.id }, JWT_SECRET, { expiresIn: '7d' });
+        res.status(200).json({ token, username: user.username });
+    } catch (error) {
+        res.status(500).json({ error: 'Internal server error' });
+    }
+};
